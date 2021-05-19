e79282d496ae9d3872a00f74c4441087192c66a8
rubabanat <rubabanat1996@gmail.com>
2021-05-19 22:07:21 +0300

    lab34

--- src/components/todo/navigation.js
+++ src/components/todo/navigation.js
@@ -1,13 +1,17 @@
 import React from "react";
 import Navbar from 'react-bootstrap/Navbar';
+import Login from './../auth/login';
+import SignUp from './../auth/signup';
 
 const NavBar = () => {
   return (
     <Navbar bg="primary" variant="dark">
-    <Navbar.Brand href="#home">
-      Home
+      <Navbar.Brand href="#home">
+        Home
     </Navbar.Brand>
-  </Navbar>
+      <Login />
+      <SignUp />
+    </Navbar>
   );
 };
 
